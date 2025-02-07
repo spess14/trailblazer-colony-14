7a3886a8d2294fb53e7c908a5f5c6972cf2fa71d
Killerqu00 <47712032+Killerqu00@users.noreply.github.com>
2025-02-07 17:11:11 +0100

    Oregen reimplement (#13)
    * Revert "basic ore generation on the planet (#9)"
    
    This reverts commit 8cbb741a
    
    * this finally works now yippee

--- Content.Server/Parallax/BiomeSystem.cs
+++ Content.Server/Parallax/BiomeSystem.cs
@@ -207,11 +207,6 @@ public sealed partial class BiomeSystem : SharedBiomeSystem
             component.Layers.Add(layer);
         }
 
-        foreach (var markerLayerProtoId in template.MarkerLayers)
-        {
-            component.ForcedMarkerLayers.Add(markerLayerProtoId);
-        }
-
         if (dirty)
             Dirty(uid, component);
     }
@@ -1031,6 +1026,10 @@ public sealed partial class BiomeSystem : SharedBiomeSystem
         SetSeed(mapUid, biome, seed.Value, false);
         SetTemplate(mapUid, biome, biomeTemplate, false);
         AddComp(mapUid, biome, true);
+        foreach (var marker in biomeTemplate.MarkerLayers)
+        {
+            AddMarkerLayer(mapUid, biome, marker);
+        }
         Dirty(mapUid, biome, metadata);
 
         var gravity = EnsureComp<GravityComponent>(mapUid);
