911ff3c74f87f42b6520448c4e72c26aad4364ab
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-03-23 22:50:08 -0600

    Update publish_multi_request.py

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,7 +15,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://raleroad.xyz/cdn/"
+ROBUST_CDN_URL = "https://robustcdn.raleroad.xyz/"
 FORK_ID = "moffstation"
 
 def main():
