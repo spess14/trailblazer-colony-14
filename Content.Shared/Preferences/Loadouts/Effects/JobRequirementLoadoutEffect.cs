263d422f85207f60804958390b885c5aabd1b8d3
Centronias <me@centronias.com>
2025-03-24 23:22:48 -0700

    CVar for disabling Role Item Loadout Timers (#13)
    * for Gage <3
    (oop sign it pls, git)
    
    * o k
    
    * I am not convinced this test failure is me

--- Content.Shared/Preferences/Loadouts/Effects/JobRequirementLoadoutEffect.cs
+++ Content.Shared/Preferences/Loadouts/Effects/JobRequirementLoadoutEffect.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics.CodeAnalysis;
+using Content.Shared.CCVar; // Moffstation
 using Content.Shared.Players.PlayTimeTracking;
 using Content.Shared.Roles;
+using Robust.Shared.Configuration; // Moffstation
 using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
@@ -17,7 +19,12 @@ public sealed partial class JobRequirementLoadoutEffect : LoadoutEffect
 
     public override bool Validate(HumanoidCharacterProfile profile, RoleLoadout loadout, ICommonSession? session, IDependencyCollection collection, [NotNullWhen(false)] out FormattedMessage? reason)
     {
-        if (session == null)
+        // Moffstation - Begin - Added CVar
+        var configurationManager = collection.Resolve<IConfigurationManager>();
+        var timersDisabled = !configurationManager.GetCVar(CCVars.GameRoleLoadoutTimers);
+        // Moffstation - End
+
+        if (session == null || timersDisabled) // Moffstation
         {
             reason = FormattedMessage.Empty;
             return true;
