20b6c3b85ef467e65dce1dc7f0ce41daf0e44e28
mjarduk <66081585+mjarduk@users.noreply.github.com>
2025-04-24 21:11:13 +0300

    Early-game research system (#47)
    * Initial commit for blueprints.
    Not usable currently. Still need to pass blueprint count to the client along with some other stuff. Still need to implement finishing research projects and printing finished blueprints.
    
    * Bluebench projects can now be started
    
    * Blueprints now display their recipes in the description; Active bluebench project cannot be started again.
    
    * Item stuff progress
    
    * Done
    
    * Changed the uhhh thingamajig
    
    * Fixed some oopsies part 1
    
    * OOPSIE WOOPSIE TwT
    
    * Removed comment, made annotations prettier
    
    * Fixed the component progress stuff
    
    * Progress now updates in the user interface
    
    * Fix capitalisation
    
    * Fixed one more bug involving uhh new projects
    
    * Oopsie no. idk

---
+++ Content.Shared/Blueprint/BluebenchBoundUserInterfaceState.cs
@@ -0,0 +1,16 @@
+﻿using Content.Shared.Stacks;
+using Content.Shared.Tag;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Blueprint;
+
+[Serializable, NetSerializable]
+public sealed class BluebenchBoundUserInterfaceState(HashSet<BluebenchResearchPrototype> availableResearchEntries, BluebenchResearchPrototype? activeProject,  Dictionary<ProtoId<StackPrototype>, int> materialProgress, Dictionary<string, int> componentProgress, Dictionary<ProtoId<TagPrototype>, int> tagProgress) : BoundUserInterfaceState
+{
+    public HashSet<BluebenchResearchPrototype> AvailableResearchEntries { get; } = availableResearchEntries;
+    public BluebenchResearchPrototype? ActiveProject { get; } = activeProject;
+    public Dictionary<ProtoId<StackPrototype>, int> MaterialProgress = materialProgress;
+    public Dictionary<string, int> ComponentProgress = componentProgress;
+    public Dictionary<ProtoId<TagPrototype>, int> TagProgress = tagProgress;
+}
---
+++ Content.Shared/Blueprint/Shared.cs
@@ -0,0 +1,20 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared.Blueprint;
+
+[NetSerializable, Serializable]
+public enum BluebenchUiKey
+{
+    Key,
+}
+
+[Serializable, NetSerializable]
+public sealed class ResearchProjectStartMessage : BoundUserInterfaceMessage
+{
+    public string Id;
+
+    public ResearchProjectStartMessage(string id)
+    {
+        Id = id;
+    }
+}
