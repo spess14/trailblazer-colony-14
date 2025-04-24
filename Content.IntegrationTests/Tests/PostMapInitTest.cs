9e4ae4d6a00004daf1e9fc8924a9152dab240ab7
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-04-24 14:41:01 -0600

    New Centcomm (#113)
    * Added stuff
    
    * Removed a phone that was causing issues
    
    * Fixed the test thingy
    
    * Added comment
    
    * typo fixed
    
    * Fixed breaking it (made it a map instead of a grid)

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -47,6 +47,7 @@ namespace Content.IntegrationTests.Tests
         private static readonly string[] DoNotMapWhitelist =
         {
             "/Maps/centcomm.yml",
+            "/Maps/_Harmony/centcomm.yml", // Harmony Centcomm
             "/Maps/bagel.yml", // Contains mime's rubber stamp --> Either fix this, remove the category, or remove this comment if intentional.
             "/Maps/gate.yml", // Contains positronic brain and LSE-1200c "Perforator"
             "/Maps/meta.yml", // Contains warden's rubber stamp
