e88057c8fe584fb122e71bbfb9189c3cf9697de6
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-04-30 13:07:52 -0600

    Handheld radio upgrade + red phone comms (#133)
    * added stuff
    
    * Added more stuff
    
    * Added comments and other stuff
    
    * It works holy shit
    
    * fixing yaml stuff
    
    * more yaml fixes
    
    * yaml fixed?
    
    * removed a broken thing from elkridge
    
    * Added comments

--- Content.Server/Radio/EntitySystems/RadioDeviceSystem.cs
+++ Content.Server/Radio/EntitySystems/RadioDeviceSystem.cs
@@ -218,7 +218,7 @@ public sealed class RadioDeviceSystem : EntitySystem
             ("originalName", nameEv.VoiceName));
 
         // log to chat so people can identity the speaker/source, but avoid clogging ghost chat if there are many radios
-        _chat.TrySendInGameICMessage(uid, args.Message, InGameICChatType.Whisper, ChatTransmitRange.GhostRangeLimit, nameOverride: name, checkRadioPrefix: false);
+        _chat.TrySendInGameICMessage(uid, args.Message, component.ChatType, ChatTransmitRange.GhostRangeLimit, nameOverride: name, checkRadioPrefix: false);    // Moffstation - Added component-dependent chatType
     }
 
     private void OnIntercomEncryptionChannelsChanged(Entity<IntercomComponent> ent, ref EncryptionChannelsChangedEvent args)
