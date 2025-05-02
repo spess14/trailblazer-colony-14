710c3b94df61e282cd5735765749d772b0389d6d
mjarduk <66081585+mjarduk@users.noreply.github.com>
2025-05-02 21:45:17 +0300

    Bluebench stuff part 2 (#75)
    * Completed blueprints can now be printed.
    Also removed unnecessary buttons and replaced it with a label.
    
    * Added the "required" field for prototypes.
    This allows us to make a big ass tree
    
    * Oops

--- Content.Shared/Blueprint/BluebenchBoundUserInterfaceState.cs
+++ Content.Shared/Blueprint/BluebenchBoundUserInterfaceState.cs
@@ -6,11 +6,13 @@ using Robust.Shared.Serialization;
 namespace Content.Shared.Blueprint;
 
 [Serializable, NetSerializable]
-public sealed class BluebenchBoundUserInterfaceState(HashSet<BluebenchResearchPrototype> availableResearchEntries, BluebenchResearchPrototype? activeProject,  Dictionary<ProtoId<StackPrototype>, int> materialProgress, Dictionary<string, int> componentProgress, Dictionary<ProtoId<TagPrototype>, int> tagProgress) : BoundUserInterfaceState
+public sealed class BluebenchBoundUserInterfaceState(HashSet<BluebenchResearchPrototype> availableResearchEntries, BluebenchResearchPrototype? activeProject,  Dictionary<ProtoId<StackPrototype>, int> materialProgress, Dictionary<string, int> componentProgress, Dictionary<ProtoId<TagPrototype>, int> tagProgress, int blueprintCount, HashSet<BluebenchResearchPrototype> researchedPrototypes) : BoundUserInterfaceState
 {
     public HashSet<BluebenchResearchPrototype> AvailableResearchEntries { get; } = availableResearchEntries;
     public BluebenchResearchPrototype? ActiveProject { get; } = activeProject;
     public Dictionary<ProtoId<StackPrototype>, int> MaterialProgress = materialProgress;
     public Dictionary<string, int> ComponentProgress = componentProgress;
     public Dictionary<ProtoId<TagPrototype>, int> TagProgress = tagProgress;
+    public HashSet<BluebenchResearchPrototype> ResearchedPrototypes = researchedPrototypes;
+    public int BlueprintCount = blueprintCount;
 }
--- Resources/Locale/en-US/_tc14/bluebench.ftl
+++ Resources/Locale/en-US/_tc14/bluebench.ftl
@@ -1,3 +1,11 @@
 bluebench-active-project = Active project:
 blueprint-description = A blueprint for some machine. It can be inserted into an autolathe.
 blueprint-contains = This blueprint contains the following recipes:
+bluebench-available-tab = Available Research
+bluebench-completed-tab = Completed Research
+bluebench-blueprints-text = {$blueprintCount ->
+    [one] one blueprint inserted
+    *[other] {$blueprintCount} blueprints inserted
+}
+bluebench-btn-start = Start
+bluebench-btn-print = Print
