a06adf4aecdc8ab981d9018a6c5d1d2548f339bc
Aearo-Deepwater <77557990+Aearo-Deepwater@users.noreply.github.com>
2025-08-18 23:41:00 -0600

    2Fort for Gun Game (#448)
    * Add the whole dang map
    
    Atomic commits? Never heard of 'em. :godo:
    
    * Add the whole dang map prototype
    
    * Add the whole dang gravity generator
    
    Also some other minor fixes and tweaks.
    
    * Add the whole dang map to the integration test list
    
    * Add the whole dang map to the whole dang map pool
    
    * Add the whole dang correct wiring for the whole dang grav generator
    
    * Fix funky whitespace
    
    Gotta make sure the whole dang file is consistent, you know?
    
    ---------
    
    Signed-off-by: Centronias <charlie.t.santos@gmail.com>
    Co-authored-by: Centronias <charlie.t.santos@gmail.com>

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -97,6 +97,7 @@ namespace Content.IntegrationTests.Tests
             "Meta",
             "MW-Dock",
             "CS-Dust2",
+            "TF2-2Fort",
             "WaterVapour",
             "Frezon",
             "Prime",
