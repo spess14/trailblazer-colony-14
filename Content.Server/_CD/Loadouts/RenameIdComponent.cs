16c0c3d0278b4bce7fcd2c5754cf87006a7b9354
Nyxilath <colton.malone@gmail.com>
2025-09-17 01:58:57 -0400

    Job Title Loadouts (#528)
    * Cherry-picks from CD
    
    * add new import, corrected string type
    
    * corrected roboticist pda visuals
    
    * one step closer
    
    * removed a few things, roboticist is now working? too tired to keep working, I'll do more tomorrow.
    
    * cleanup, defined job titles senior and intern positions
    
    * removed intern roles from map pools
    
    * fix typo
    
    * dammit, my plan to secretly add clowns to superlowpop is causing build failures
    
    * relisted intern PDA changes under moffstation
    
    * role compensation adjustment

---
+++ Content.Server/_CD/Loadouts/RenameIdComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace Content.Server._CD.Loadouts;
+
+/// <summary>
+/// Marker that should be attached to the PDA to rename the contained ID to the user's requested job. Used to implement custom tile loadouts.
+/// </summary>
+[RegisterComponent]
+public sealed partial class RenameIdComponent : Component
+{
+    [DataField]
+    public LocId Value;
+}
--- Resources/Locale/en-US/_Moffstation/job/job-names.ftl
+++ Resources/Locale/en-US/_Moffstation/job/job-names.ftl
@@ -1,1 +1,5 @@
 job-name-mercenary = Mercenary
+job-name-senior-researcher = Senior Researcher
+job-name-senior-physician = Senior Physician
+job-name-senior-officer = Senior Officer
+job-name-senior-courier = Senior Courier
