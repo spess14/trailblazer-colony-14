05dda6b707247dcc8b0ee7681f5ac7a4cb3e6da6
Jona-K <63449674+Katzenminer@users.noreply.github.com>
2026-01-17 05:59:50 +0100

    Add a SetCountdown Command to set the Countdown till roundstart (#965)
    * FML this was supposed to take seconds not hours....
    
    * Update Content.Server/GameTicking/GameTicker.RoundFlow.cs
    
    Signed-off-by: Jona-K <63449674+Katzenminer@users.noreply.github.com>
    
    * Update Content.Server/GameTicking/GameTicker.RoundFlow.cs
    
    Signed-off-by: Jona-K <63449674+Katzenminer@users.noreply.github.com>
    
    * Adjusted Command discription
    
    * Apply suggestions from code review
    
    fair enough
    
    Co-authored-by: Centronias <charlie.t.santos@gmail.com>
    Signed-off-by: Jona-K <63449674+Katzenminer@users.noreply.github.com>
    
    * i was unable to see why the test failed so we try again before i go insane
    
    * Changed Dependency injection
    
    * You say nothin *smoke bomb*
    
    ---------
    
    Signed-off-by: Jona-K <63449674+Katzenminer@users.noreply.github.com>
    Co-authored-by: Centronias <charlie.t.santos@gmail.com>

--- Content.Server/GameTicking/GameTicker.RoundFlow.cs
+++ Content.Server/GameTicking/GameTicker.RoundFlow.cs
@@ -797,6 +797,22 @@ namespace Content.Server.GameTicking
             return true;
         }
 
+        // Moffstation - Start - SetCountdown Command
+        public bool SetCountdown(TimeSpan time)
+        {
+            if (_runLevel != GameRunLevel.PreRoundLobby) // must be in preround
+                return false;
+
+            _roundStartTime = _gameTiming.CurTime + time;
+            RaiseNetworkEvent(new TickerLobbyCountdownEvent(_roundStartTime, Paused));
+            _chatManager.DispatchServerAnnouncement(
+                Loc.GetString("game-ticker-set-countdown", ("seconds", time.TotalSeconds))
+            );
+
+            return true;
+        }
+        // Moffstation - End
+
         private void UpdateRoundFlow(float frameTime)
         {
             if (RunLevel == GameRunLevel.InRound)
