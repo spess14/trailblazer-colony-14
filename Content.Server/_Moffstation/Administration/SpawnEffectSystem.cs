237aef4ea3ddbe128486b8d774db38f9a4f203de
Jona-K <63449674+Katzenminer@users.noreply.github.com>
2026-01-18 04:37:33 +0100

    new command ToggleSpawnEffect (#968)
    * WAWA
    
    * Done perhaps maybe most likely
    
    * Removing Crazy comments, ... yes i struggeld with that flag
    
    * Huzza there was an event im stupid woops
    
    * wawa revert on t he sandbox system
    
    * The Wawa To rule em all:WAWAWAWAWAWAWAWAWAWAWA
    
    * one more wawa
    
    * wawa?
    
    * Apply suggestions from code review
    
    wawa
    
    Co-authored-by: Centronias <charlie.t.santos@gmail.com>
    Signed-off-by: Jona-K <63449674+Katzenminer@users.noreply.github.com>
    
    * Final wawa :3 perhaps
    
    * evil overlord forced this  and hates scurrets
    
    Co-authored-by: Centronias <charlie.t.santos@gmail.com>
    Signed-off-by: Jona-K <63449674+Katzenminer@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Jona-K <63449674+Katzenminer@users.noreply.github.com>
    Co-authored-by: Centronias <charlie.t.santos@gmail.com>

---
+++ Content.Server/_Moffstation/Administration/SpawnEffectSystem.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+using Robust.Shared.Console;
+using Robust.Shared.Enums;
+using Robust.Shared.Placement;
+using Robust.Shared.Network;
+using Robust.Shared.Player;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._Moffstation.Administration;
+
+public sealed class SpawnEffectSystem : EntitySystem
+{
+    [Dependency] private readonly ISharedPlayerManager _playerManager = default!;
+    [Dependency] private readonly IPrototypeManager _proto = default!;
+
+    private readonly Dictionary<NetUserId, EntProtoId> _activeEffectsByUser = new();
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<PlacementEntityEvent>(OnPlace);
+
+        _playerManager.PlayerStatusChanged += OnPlayerStatusChanged;
+    }
+
+    public override void Shutdown()
+    {
+        base.Shutdown();
+        _playerManager.PlayerStatusChanged -= OnPlayerStatusChanged;
+    }
+
+    public bool TrySetEffect(NetUserId user, string? effectId)
+    {
+        if (effectId is null) {
+            ClearEffect(user);
+            return true;
+        }
+
+        if (!_proto.HasIndex<EntityPrototype>(effectId))
+            return false;
+
+        SetEffect(user, effectId);
+        return true;
+    }
+
+    public void ClearEffect(NetUserId user)
+    {
+        _activeEffectsByUser.Remove(user);
+    }
+
+    public void SetEffect(NetUserId user, [ForbidLiteral] EntProtoId effectId)
+    {
+        if (!_activeEffectsByUser.ContainsKey(user))
+        {
+            _activeEffectsByUser.Add(user, effectId);
+        }
+        _activeEffectsByUser[user] = effectId;
+
+    }
+
+    public IEnumerable<CompletionOption> GetEffects()
+    {
+        return _proto.EnumeratePrototypes<EntityPrototype>()
+            .Where(p => p.ID.StartsWith("AdminInstantEffect"))
+            .Select(p => new CompletionOption(p.ID, p.Name))
+            .OrderBy(o => o.Value);
+    }
+
+    private void OnPlace(PlacementEntityEvent args)
+    {
+        // You have to be "creating something" and need to be a player cause server cant place stuff wawa
+        if (args.PlacementEventAction != PlacementEventAction.Create || args.PlacerNetUserId == null)
+            return;
+
+        if (_activeEffectsByUser.TryGetValue(args.PlacerNetUserId.Value, out var effect))
+        {
+            Spawn(effect, args.Coordinates);
+        }
+    }
+
+    private void OnPlayerStatusChanged(object? sender, SessionStatusEventArgs e)
+    {
+        if (e.NewStatus == SessionStatus.Disconnected)
+        {
+            _activeEffectsByUser.Remove(e.Session.UserId);
+        }
+    }
+}
