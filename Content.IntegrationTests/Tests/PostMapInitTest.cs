fd0b8d942c8599308417f50847ff2058133fc774
Aearo-Deepwater <77557990+Aearo-Deepwater@users.noreply.github.com>
2026-02-03 14:13:15 -0700

    Add Skeld as an admin-selectable map (#771)
    * Add Skeld files to Moffstation
    
    * Make minor tweaks
    
    Especially for replacing generic machine module parts with capacitors
    and matter bins.
    
    * Tweak population numbers
    
    * Fix prototype's paths to the maps
    
    * Add Skeld to map integration test list
    
    * Fix skeld parallax prototype
    
    * Update Resources/Prototypes/_Moffstation/Maps/skeld.yml
    
    Co-authored-by: Southbridge <7013162+southbridge-fur@users.noreply.github.com>
    Signed-off-by: Aearo-Deepwater <77557990+Aearo-Deepwater@users.noreply.github.com>
    
    * Various tweaks
    
    Largely concerning the swapping-out of entities that had their storage
    fill components removed with ones that didn't have them to begin with.
    
    * Make crayon boxes the right ones for good hopefully
    
    * Update Content.IntegrationTests/Tests/PostMapInitTest.cs
    
    Co-authored-by: DuckManZach <144298822+DuckManZach@users.noreply.github.com>
    Signed-off-by: Aearo-Deepwater <77557990+Aearo-Deepwater@users.noreply.github.com>
    
    * Remove assistant roles
    
    * Add Skeld to default rotation
    
    ---------
    
    Signed-off-by: Aearo-Deepwater <77557990+Aearo-Deepwater@users.noreply.github.com>
    Co-authored-by: Southbridge <7013162+southbridge-fur@users.noreply.github.com>
    Co-authored-by: DuckManZach <144298822+DuckManZach@users.noreply.github.com>

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -73,6 +73,7 @@ namespace Content.IntegrationTests.Tests
             {"/Maps/_Moffstation/Nonstations/d1_logic.yml", ["ShuttleGunSvalinnMachineGun"]},
             {"/Maps/_Moffstation/frezon.yml", ["HandheldCrewMonitor"]},
             {"/Maps/_Moffstation/dugstation.yml", ["ClothingHeadHatCatEarsValid"]},
+            {"/Maps/_Moffstation/skeld.yml", ["ShuttleGunDuster"]},
             // Moffstation - End
         };
 
@@ -151,6 +152,7 @@ namespace Content.IntegrationTests.Tests
             "Aspid",
             "Elkridge",
             "Wapiti",
+            "Skeld"
             // Moffstation - End
         };
 
