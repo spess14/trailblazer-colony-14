068d5c1347c8c7ed3ca63fe4200b319d8daa06fd
murphyneko <killerqueen1777@gmail.com>
2026-02-25 16:33:42 +0000

    Campfire QoL and changes (#103)
    ## About + Why
    Campfire now heats up multiple items on it, but it splits the heat between them all, as well as now having a maximum temperature it can heat up things to (350K). If you want to brew advanced chemicals, you'll need a hotplate.
    Also lowered the bioplastic reaction temperature from 374 to 350 - that doesn't affect the regular plastic reaction, I have different plans for that.
    
    ## Technical details
    FueledHeater now no longer sends events to server-side in an update loop what the fuck
    
    ## Media
    <!-- Attach media if the PR makes in-game changes (clothing, items, features, etc).
    Small fixes/refactors are exempt. -->
    
    ## Requirements
    <!-- Confirm the following by placing an X in the brackets without spaces inside (for example: [X] ): -->
    - [x] I have read and am following the [Upstream Pull Request and Changelog Guidelines](https://docs.spacestation14.com/en/general-development/codebase-info/pull-request-guidelines.html) as well as the [TC14 Contributing Guidelines](https://git.tc14.space/tc14/trailblazer-colony-14/src/branch/master/CONTRIBUTING.md).
    - [x] I have properly sectioned my changes into fork namespaces, and/or followed proper guidelines for modifying upstream files.
    - [x] I have added media to this PR or it does not require an in-game showcase.
    <!-- You should understand that not following the above may get your PR closed at maintainer’s discretion -->
    
    ## Breaking changes
    <!-- List any breaking changes, including namespaces, public class/method/field changes, prototype renames - especially if they're done in upstream code. -->
    
    **Changelog**
    <!-- Add a Changelog entry to make players aware of new features or changes that could affect gameplay.
    Make sure to read the guidelines and take this Changelog template out of the comment block in order for it to show up.
    Changelog must have a :cl: symbol, so the bot recognizes the changes and adds them to the game's changelog. -->
    <!--
    :cl:
    - add: Added fun!
    - remove: Removed fun!
    - tweak: Changed fun!
    - fix: Fixed fun!
    -->
    
    Reviewed-on: https://git.tc14.space/tc14/trailblazer-colony-14/pulls/103
    Co-authored-by: murphyneko <killerqueen1777@gmail.com>
    Co-committed-by: murphyneko <killerqueen1777@gmail.com>

--- Content.Shared/_tc14/Chemistry/Components/FueledHeaterComponent.cs
+++ Content.Shared/_tc14/Chemistry/Components/FueledHeaterComponent.cs
@@ -5,12 +5,24 @@ namespace Content.Shared._tc14.Chemistry.Components;
 /// <summary>
 /// Hybrid solution-item heater.
 /// </summary>
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class FueledHeaterComponent : Component
 {
-    [DataField]
+    /// <summary>
+    /// How much heat solutions get per second.
+    /// </summary>
+    [DataField, AutoNetworkedField]
     public float SolutionHeatPerSecond;
 
-    [DataField]
+    /// <summary>
+    /// How much heat entities get per second (important for stuff like cooking meat)
+    /// </summary>
+    [DataField, AutoNetworkedField]
     public float EntityHeatPerSecond;
+
+    /// <summary>
+    /// Maximum temperature the heater can heat up things to.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public int MaxTemp = 350;
 }
--- Content.Shared/_tc14/Chemistry/Systems/FueledHeaterSystem.cs
+++ Content.Shared/_tc14/Chemistry/Systems/FueledHeaterSystem.cs
@@ -2,6 +2,8 @@ using Content.Shared._tc14.Chemistry.Components;
 using Content.Shared.Chemistry.Components.SolutionManager;
 using Content.Shared.Chemistry.EntitySystems;
 using Content.Shared.Placeable;
+using Content.Shared.Temperature.Components;
+using Content.Shared.Temperature.Systems;
 
 namespace Content.Shared._tc14.Chemistry.Systems;
 
@@ -11,6 +13,7 @@ namespace Content.Shared._tc14.Chemistry.Systems;
 public sealed class FueledHeaterSystem : EntitySystem
 {
     [Dependency] private readonly SharedSolutionContainerSystem _solution = default!;
+    [Dependency] private readonly SharedTemperatureSystem _temperature = default!;
 
     public override void Update(float frameTime)
     {
@@ -25,26 +28,23 @@ public sealed class FueledHeaterSystem : EntitySystem
 
     private void UpdateHeater(EntityUid uid, FueledHeaterComponent heater, ItemPlacerComponent placer, float frameTime)
     {
+        var entityCount = placer.PlacedEntities.Count;
         foreach (var heatingEntity in placer.PlacedEntities)
         {
             if (!TryComp<SolutionContainerManagerComponent>(heatingEntity, out var container))
                 continue;
 
-            var solutionEnergy = heater.SolutionHeatPerSecond * frameTime;
+            var solutionEnergy = heater.SolutionHeatPerSecond * frameTime / entityCount;
             foreach (var (_, soln) in _solution.EnumerateSolutions((heatingEntity, container)))
             {
-                _solution.AddThermalEnergy(soln, solutionEnergy);
+                _solution.AddThermalEnergyClamped(soln, solutionEnergy, 0, heater.MaxTemp);
             }
         }
-        var entityEnergy = heater.EntityHeatPerSecond * frameTime;
+        var entityEnergy = heater.EntityHeatPerSecond * frameTime / entityCount;
         foreach (var ent in placer.PlacedEntities)
         {
-            var ev = new HeatEntityEvent
-            {
-                EntityUid = ent,
-                Energy = entityEnergy,
-            };
-            RaiseLocalEvent(ref ev);
+            if (TryComp<TemperatureComponent>(ent, out var temperature) && temperature.CurrentTemperature < heater.MaxTemp)
+                _temperature.ChangeHeat(ent, entityEnergy);
         }
     }
 }
