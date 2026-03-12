8b838060c8a09d3c960195f345e120b65962a830
Kyle Tyo <36606155+VerinSenpai@users.noreply.github.com>
2026-03-12 14:03:35 -0400

    Fix character saving being blocked by invalid hair fields. (#1146)
    commit

--- Content.Shared/Humanoid/Markings/MarkingManager.cs
+++ Content.Shared/Humanoid/Markings/MarkingManager.cs
@@ -175,13 +175,16 @@ public sealed class MarkingManager
     /// </summary>
     public void EnsureValidLayers(Dictionary<HumanoidVisualLayers, List<Marking>> markingSets, HashSet<HumanoidVisualLayers> layers)
     {
-        foreach (var markings in markingSets.Values)
+        foreach (var (markingSet, markings) in markingSets) // MoffStation
         {
             for (var i = markings.Count - 1; i >= 0; i--)
             {
                 if (!TryGetMarking(markings[i], out var marking) || !layers.Contains(marking.BodyPart))
                     markings.RemoveAt(i);
             }
+            // MoffStation Start
+            if (markings.Count == 0)
+                markingSets.Remove(markingSet); // MoffStation End
         }
     }
 
