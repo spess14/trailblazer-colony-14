6a61302795e56d6b60cd87d79e388decf2201a32
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2026-06-10 06:37:38 -0600

    New Map: Dove station (Harmony Port) (#1516)
    * New Map: Dove Station (Hamroy Port)
    
    * Migrations
    
    * Role and pop changes
    
    * Guard comments

--- Content.IntegrationTests/Tests/Power/StationPowerTests.cs
+++ Content.IntegrationTests/Tests/Power/StationPowerTests.cs
@@ -45,6 +45,7 @@ public sealed class StationPowerTests : GameTest
         "Prime", // Moffstation (From Harmony)
         "Scylla", // Moffstation (From Harmony)
         "Wapiti", // Moffstation (From Harmony)
+        "Dove", // Moffstation (From Harmony)
     ];
 
     public override PoolSettings PoolSettings => new ()
